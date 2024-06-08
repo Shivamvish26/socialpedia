fbfeb33102faaff48a4e227de7fa5bf2d761b45b
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-06-08 17:04:32 +0530

    server files changes

--- server/index.js
+++ server/index.js
@@ -8,13 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import {createPost} from "./controllers/posts.js"
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-
+import User from "./models/User.js"
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -48,6 +50,10 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // ADDED DATA ONE TIME
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
@@ -57,5 +63,5 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // Routes
 app.use("/auth", authRoutes);
-app.use("/users",userRoutes);
-app.use("/posts",postRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
