ed6adc6014515769b2a9f544091464839a7459d3
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-06-08 15:44:12 +0530

    small changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,18 @@
 import express from "express";
 import {
-    getUser,
+    getUsers,
     getUserFriends,
-    addRemoveFriends,
+    addRemoveFriend,
 }from '../controllers/users.js'
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // READ
-router.get('/:id', verifyToken, getUser);
+router.get('/:id', verifyToken, getUsers);
 router.get('/:id/friends', verifyToken, getUserFriends);
 
 // UPDATE
-router.patch('/:id/:friendId',verifyToken, addRemoveFriends);
+router.patch('/:id/:friendId',verifyToken, addRemoveFriend);
 
 export default router;
